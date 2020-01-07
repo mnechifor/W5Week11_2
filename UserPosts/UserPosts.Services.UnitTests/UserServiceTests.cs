a3f2a08a10de5e4296f7a4d7b5bece2ca19c1f72
Mihai Nechifor <Mihai.Nechifor@endava.com>
2020-01-07 21:00:02 +0200

    upd

--- UserPosts/UserPosts.Services.UnitTests/UserServiceTests.cs
+++ UserPosts/UserPosts.Services.UnitTests/UserServiceTests.cs
@@ -1,17 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using NSubstitute;
-using UserPosts.Domain;
-
-namespace UserPosts.Services.UnitTests
+﻿namespace UserPosts.Services.UnitTests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using NSubstitute;
+    using UserPosts.Domain;
+
     [TestFixture]
     public class UserServiceTests
     {
+        private IUserRepository fakeUserRepository;
+        private User fakeUser;
+        private List<Post> dummyPosts;
+
+        [SetUp]
+        public void Setup()
+        {
+            fakeUserRepository = Substitute.For<IUserRepository>();
+
+            fakeUser = new User()
+            {
+                Email = "fake@fake.com",
+                Id = 1,
+                Name = "name",
+                Username = "username"
+            };
+
+            dummyPosts = new List<Post>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                dummyPosts.Add(new Post()
+                {
+                    UserId = 1,
+                    Body = "asdsa"
+                });
+            }
+        }
+
         [Test]
         public void Should_Have_Inactive_Status_When_Less_Than_Five_Posts()
         {
@@ -32,40 +61,14 @@ namespace UserPosts.Services.UnitTests
         public void Should_Have_Inactive_Status_When_Less_Than_Five_Posts2()
         {
             //Arrange
-            var fakeUserRepository = Substitute.For<IUserRepository>();
-
-            var fakeUser = new User()
-            {
-                Email = "fake@fake.com",
-                Id = 1,
-                Name = "name",
-                Username = "username"
-            };
 
             fakeUserRepository.GetById(1).Returns(fakeUser);
 
             var fakePostRepository = Substitute.For<IPostRepository>();
 
-            var dummyPosts = new List<Post>()
-            {
-                new Post()
-                {
-                    UserId = 1,
-                    Body = "asdsa"
-                },
-                new Post()
-                {
-                    UserId = 1,
-                    Body = "body 2"
-                },
-                new Post()
-                {
-                    UserId = 1,
-                    Body = "body 2"
-                }
-            };
+            var fakePosts = dummyPosts.Take(4).ToList();
 
-            fakePostRepository.GetPostsByUserId(1).Returns(dummyPosts);
+            fakePostRepository.GetPostsByUserId(1).Returns(fakePosts);
 
             UserService sut = new UserService(fakeUserRepository, fakePostRepository);
             //Act
@@ -74,6 +77,27 @@ namespace UserPosts.Services.UnitTests
 
             //Assert
             Assert.AreEqual(UserPostsStatus.Inactive, response.Status);
+        }
+
+        [Test]
+        public void Should_Have_Inactive_Status_When_Less_Than_Five_Posts3()
+        {
+            //Arrange
+            fakeUserRepository.GetById(1).Returns(fakeUser);
+
+            var fakePostRepository = Substitute.For<IPostRepository>();
+
+            var fakePosts = dummyPosts.Take(7).ToList();
+
+            fakePostRepository.GetPostsByUserId(1).Returns(fakePosts);
+
+            UserService sut = new UserService(fakeUserRepository, fakePostRepository);
+            //Act
+
+            var response = sut.GetUserActiveRespose(1);
+
+            //Assert
+            Assert.AreEqual(UserPostsStatus.Active, response.Status);
         }
     }
 }
