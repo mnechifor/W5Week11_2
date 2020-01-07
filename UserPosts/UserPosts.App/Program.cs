ff408ffb0fc7cc5d73082002dbf20c01bde02e17
Mihai Nechifor <Mihai.Nechifor@endava.com>
2020-01-07 16:31:59 +0200

    upd

---
+++ UserPosts/UserPosts.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using UserPosts.Data;
+using UserPosts.Domain;
+using UserPosts.Services;
+
+namespace UserPosts.App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IPostRepository postRepository = new PostDataAccess();
+            IUserRepository userRepository = new UserDataAccess();
+
+            var service = new UserService(userRepository, postRepository);
+
+            var response = service.GetUserActiveRespose(2);
+        }
+    }
+}
